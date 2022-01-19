2e0b5a919f4b675ff2c3d6b56f13ec50e620055d
Denys <SenekaGl@gmail.com>
2022-01-19 17:15:13 +0200

    test

--- main.py
+++ main.py
@@ -35,20 +35,28 @@ def start(message):
     update_messages_count(user_id)
 
 
-@bot.message_handler(commands=["stats"])
-def get_stats(message):
-    db_object.execute("SELECT * FROM users ORDER BY messanges DESC LIMIT 10")
-    result = db_object.fetchall()
-
-    if not result:
-        bot.reply_to(message, "No data...")
-    else:
-        reply_message = "- Top flooders:\n"
-        for i, item in enumerate(result):
-            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
-        bot.reply_to(message, reply_message)
-
+@bot.message_handler(commands=["day"])
+def get_week_days(message):
+    db_object.execute("SELECT * FROM day")
+    data = db_object.fetchall()
+    for i in range(data):
+        bot.reply_to(message, data)
     update_messages_count(message.from_user.id)
+
+# @bot.message_handler(commands=["stats"])
+# def get_stats(message):
+#     db_object.execute("SELECT * FROM users ORDER BY messanges DESC LIMIT 10")
+#     result = db_object.fetchall()
+#
+#     if not result:
+#         bot.reply_to(message, "No data...")
+#     else:
+#         reply_message = "- Top flooders:\n"
+#         for i, item in enumerate(result):
+#             reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
+#         bot.reply_to(message, reply_message)
+#
+#     update_messages_count(message.from_user.id)
 
 @bot.message_handler(func=lambda message: True, content_types=["text"])
 def message_from_user(message):
