0cde9a1258af83f009ab835dfa905f523ddec860
Denys <SenekaGl@gmail.com>
2022-01-19 04:43:37 +0200

    created test echo bot

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 bot_token = "5098531929:AAE6JThL5yRwXJfSKwF2atzBnb-yT9nyu-4"
-admin_id = 397085287
+APP_URL = f'https://parapam.herokuapp.com/{bot_token}'
 
--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-import asyncio
-from aiogram import Bot, Dispatcher, executor
-from config import bot_token
-# Подключаем асинхронную библеотеку бот API
+import os
+import telebot
+from config import APP_URL, bot_token
+from flask import Flask, request
 
-loop = asyncio.get_event_loop()
-bot = Bot(bot_token, parse_mode="HTML")
-dp = Dispatcher(bot, loop=loop)
+bot = telebot.TeleBot(bot_token)
+server = Flask(__name__)
 
-if __name__ == "__main__":
-    from handlers import dp, send_to_admin
-    executor.start_polling(dp, on_startup=send_to_admin)
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello ' + message.from_user.first_name)
 
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo(messange):
+    bot.reply_to(messange, messange.text)
+
+
+@server.route('/' + bot_token, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
