277c78eb25c796f462d637cabdbfd7a8f36aff51
Denys <SenekaGl@gmail.com>
2022-01-19 05:12:25 +0200

    Add base logic bot

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 import os
 import telebot
+import logging
+from config import *
 from flask import Flask, request
-bot_token = "5098531929:AAE6JThL5yRwXJfSKwF2atzBnb-yT9nyu-4"
-APP_URL = f'https://parapam.herokuapp.com/{bot_token}'
-bot = telebot.TeleBot(bot_token)
+
+bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
+
+# We guarantee that the server will start only with a direct call to the script __main__
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello ' + message.from_user.first_name)
+    username = message.from_user.usermname
+    bot.reply_to(message, f'Hello , {username}!')
 
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo(messange):
-    bot.reply_to(messange, messange.text)
-
-
-@server.route('/' + bot_token, methods=['POST'])
-def get_message():
+# Redirect from the Flask server to the bot
+@server.route('/' + BOT_TOKEN, methods=['POST'])
+def redirect_message():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return '!', 200
 
 
-@server.route('/')
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    return '!', 200
 
-if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+
+
+
