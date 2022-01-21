7d347f450dd336637829b48b348f5364d3187a19
Denys <SenekaGl@gmail.com>
2022-01-21 05:45:03 +0200

    Update Procfile and config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 BOT_TOKEN = "5084334710:AAEvgocyKbDOprxRLpgKiEnCPJ37cS3APGI"
 APP_URL = "https://parapam.herokuapp.com/" + BOT_TOKEN
 DB_URI = "postgres://yxlhuhheochrud:02c7ac751321274f183bd774ccf78d28cc47e4b1f292c8ab7200fb0233c42a4f@ec2-52-31-201-170.eu-west-1.compute.amazonaws.com:5432/dd3m3v9don3i3j"
+
